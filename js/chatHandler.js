e86b49f87e75779db1f9614ebfae31089b70635d
Nick <3playgames99@gmail.com>
2018-02-08 09:58:55 -0800

    Get chat handler to a working state

--- js/chatHandler.js
+++ js/chatHandler.js
@@ -3,7 +3,9 @@ var chatSocket;
 function connectToChat(id) {
     $.get("https://mixer.com/api/v1/chats/" + id + "/anonymous", (data) => { }).done((data) => {
         //Connect to chat
-        chatSocket = new WebSocket(data.endpoints[generateRandNumb(0, data.endpoints.length)]);
+        var num = generateRandNumb(0, data.endpoints.length);
+        console.log(num);
+        chatSocket = new WebSocket(data.endpoints[num]);
 
         chatSocket.addEventListener('open', (event) => {
             console.log("Connected to chat socket");
@@ -15,10 +17,31 @@ function connectToChat(id) {
         });
 
         chatSocket.addEventListener('message', (event) => {
-            console.log('Message from server', event.data);
+            //Listen to different events sent
+            var data = JSON.parse(event.data);
+            console.log(data);
+            if(data.type == "event") {
+                if(data.event == "WelcomeEvent") {
+                    var authPacket = {
+                        "type": "method",
+                        "method": "auth",
+                        "arguments": [
+                           id
+                        ],
+                        "id": 0
+                     }
+
+                     chatSocket.send(JSON.stringify(authPacket));
+                }
+            }
+
         });
 
     }).fail((data) => {
         alert("Failed to connect to chat.");
     });
+}
+
+function closeChat(){
+    chatSocket.close();
 }
